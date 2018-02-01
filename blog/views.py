b6a13ec42d0d0832275069e135a7cfee7de32b94
yinzhou <524469942@qq.com>
2018-02-01 21:22:11 +0800

    2018-2-1 Third try

--- blog/views.py
+++ blog/views.py
@@ -204,16 +204,16 @@ class   TagView(ListView):
 
 
 
-def search(request):
-    q = request.GET.get('q')
-    error_msg = ''
-
-    if not q:
-        error_msg = '请输入关键词'
-        return render(request,'blog/index.html',{'error_msg':error_msg})
-    post_list = Post.objects.filter(Q(title__icontains=q)   |   Q(body__icontains=q))
-    return render(request,'blog/index.html',{'error_msg':error_msg},
-                  {'post_list':post_list})
+# def search(request):
+#     q = request.GET.get('q')
+#     error_msg = ''
+#
+#     if not q:
+#         error_msg = '请输入关键词'
+#         return render(request,'blog/index.html',{'error_msg':error_msg})
+#     post_list = Post.objects.filter(Q(title__icontains=q)   |   Q(body__icontains=q))
+#     return render(request,'blog/index.html',{'error_msg':error_msg},
+#                   {'post_list':post_list})
 
 
 
