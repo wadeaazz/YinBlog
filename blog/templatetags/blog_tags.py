5e5d77605aaab1557fec72c20d88dbb93ac37206
yinzhou <524469942@qq.com>
2018-02-01 16:44:07 +0800

    2018-2-1 First try

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -18,4 +18,4 @@ def get_categories():
 
 @register.simple_tag
 def get_tags():
-    return Tag.objects.annotate(num_posts=Count('post').filter(num_posts__gt=0))
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
